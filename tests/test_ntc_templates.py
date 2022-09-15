6d24e2cdb30c976ec10a2880691c0e6a2f219886
Elin <365433079@qq.com>
2022-09-15 16:28:34 +0800

    ✨ feat: 增加版本号测试

---
+++ tests/test_ntc_templates.py
@@ -0,0 +1,12 @@
+from ntc_templates import __version__
+import os.path as p
+
+
+def test_version():
+    ver = "3.3.2"
+
+    assert __version__ == ver
+    setup_file = p.join(p.dirname(p.dirname(__file__)), "pyproject.toml")
+    with open(setup_file, "r", encoding="utf8") as f:
+        content = f.read()
+        assert f'version = "{ver}"' in content
