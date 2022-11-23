189e455db3854b257a99be5c84754f4829d77125
Elinpf <365433079@qq.com>
2022-11-23 23:28:12 +0800

    ✨ feat: update version to v3.4.0

--- tests/test_ntc_templates.py
+++ tests/test_ntc_templates.py
@@ -3,7 +3,7 @@ import os.path as p
 
 
 def test_version():
-    ver = "3.3.7"
+    ver = "3.4.0"
 
     assert __version__ == ver
     setup_file = p.join(p.dirname(p.dirname(__file__)), "pyproject.toml")
